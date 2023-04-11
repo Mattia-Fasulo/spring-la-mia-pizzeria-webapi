9668597ec8a9ed79566ae122a9116a4343d5e285
Mattia-Fasulo <mattia.fasulo@gmail.com>
2023-04-11 15:14:04 +0200

    Added API for details pizza

--- src/main/java/org/exercise/pizzeria/api/PizzaRestController.java
+++ src/main/java/org/exercise/pizzeria/api/PizzaRestController.java
@@ -1,9 +1,12 @@
 package org.exercise.pizzeria.api;
 
+import org.exercise.pizzeria.exceptions.PizzaNotFoundException;
 import org.exercise.pizzeria.model.Pizza;
 import org.exercise.pizzeria.services.PizzaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Optional;
@@ -16,6 +19,7 @@ public class PizzaRestController {
     @Autowired
     private PizzaService pizzaService;
 
+    // all pizzas
     @GetMapping
     public List<Pizza> list(
             @RequestParam(name = "q")Optional<String> search
@@ -24,5 +28,16 @@ public class PizzaRestController {
             return pizzaService.getByName(search.get());
         }
         return pizzaService.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public Pizza getById(
+            @PathVariable Integer id
+    ){
+        try{
+            return  pizzaService.getById(id);
+        } catch (PizzaNotFoundException e){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
     }
 }
